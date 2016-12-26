5fd79c137f7d792aee235792c0980c996fa27ca7
james <imjamespond@gmail.com>
2016-12-26 17:43:19 +0800

    testing

--- test1.py
+++ test1.py
@@ -26,20 +26,25 @@ flags.DEFINE_integer('batch_size', len(TRAINNING_IMAGE_FILES), 'Batch size. Must
 flags.DEFINE_string('train_dir', 'data', 'Directory to put the training data.')
 flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data for unit testing.')
 
-#
-train_images = []
-for filename in TRAINNING_IMAGE_FILES:
-  image = Image.open(filename)#读取image数据
-  image = image.resize((mnist.IMAGE_SIZE,mnist.IMAGE_SIZE))#resize图像
-  image = np.array(image)#通过numpy得到数组
-  train_images.append(image)
-  #print(image)
+def get_image_data(image_files):
+  train_images = []
+  for filename in image_files:
+    image = Image.open(filename)#读取image数据
+    image = image.resize((mnist.IMAGE_SIZE,mnist.IMAGE_SIZE))#resize图像
+    image = np.array(image)#通过numpy得到数组
+    train_images.append(image)
+    #print(image)
+    
+  train_images = np.array(train_images)#展开
+  train_images = train_images.reshape(len(image_files), mnist.IMAGE_PIXELS)#重塑
+  
+  label = [0,1,1]
+  train_labels = np.array(label)
+  
+  return train_images,train_labels
 
-train_images = np.array(train_images)#展开
-train_images = train_images.reshape(len(TRAINNING_IMAGE_FILES), mnist.IMAGE_PIXELS)#重塑
-
-label = [0,1,1]
-train_labels = np.array(label)
+train_images,train_labels = get_image_data(TRAINNING_IMAGE_FILES)
+test_images,test_labels = get_image_data(TESTING_IMAGE_FILES)
 
 def placeholder_inputs(batch_size, pixel_num):
   images_placeholder = tf.placeholder(tf.float32, shape=(batch_size, pixel_num))
@@ -73,6 +78,7 @@ def run_training():
   with tf.Graph().as_default():
     #持有image,label数据
     images_placeholder, labels_placeholder = placeholder_inputs(len(TRAINNING_IMAGE_FILES), mnist.IMAGE_PIXELS)
+    test_images_placeholder, test_labels_placeholder = placeholder_inputs(len(TESTING_IMAGE_FILES), mnist.IMAGE_PIXELS)
     #推测模型
     logits = mnist.inference(images_placeholder, FLAGS.hidden1, FLAGS.hidden2)
     #输出丢失计算
@@ -91,6 +97,12 @@ def run_training():
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
       feed_dict = fill_feed_dict(train_images, train_labels, images_placeholder, labels_placeholder)
+      # Run one step of the model.  The return values are the activations
+      # from the `train_op` (which is discarded) and the `loss` Op.  To
+      # inspect the values of your Ops or variables, you may include them
+      # in the list passed to sess.run() and the value tensors will be
+      # returned in the tuple from the call.运行模型一步,返回值是训练输出的激活,损失.
+      #要窥视输出值和变量,你要包在run的list中括包它们,tensors将返回一个tuple
       _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
       duration = time.time() - start_time
       
@@ -101,7 +113,7 @@ def run_training():
         checkpoint_file = os.path.join(FLAGS.train_dir, 'model.ckpt')
         saver.save(sess, checkpoint_file, global_step=step)
         print('Training Data Eval:')
-        do_eval(sess, eval_correct, images_placeholder, labels_placeholder, train_images)
+        do_eval(sess, eval_correct, test_images_placeholder, test_labels_placeholder, train_images)
 
 def main(_):
   run_training()
