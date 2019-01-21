e76d1854053e0b46b89e636de4bbe5da814d48cb
james <james_huangjian@163.com>
2019-01-21 17:27:58 +0800

    django re_path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from cctv.views import index as cctv_index
 
 urlpatterns = [
-    path('cctv/', cctv_index),
+    re_path(r'^cctv/.*$', cctv_index),
     path('api/cctv/', include('cctv.urls')),
     path('admin/', admin.site.urls),
 ]
